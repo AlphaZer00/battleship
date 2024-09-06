ca3122c528b9196312fac3b6617a44064e2b3a99
AlphaZer00 <batudasdemir@gmail.com>
2024-09-06 13:28:02 -0400

    Fix checkBounds

--- gameboard.js
+++ gameboard.js
@@ -11,18 +11,41 @@ const Gameboard = () => {
 
 	function setEndpoint(x, y, direction, length) {
 		if (direction === "horizontal") {
-			endpoint = x - 1 + length;
-		} else if (direction === "vertical") {
 			endpoint = y - 1 + length;
+		} else if (direction === "vertical") {
+			endpoint = x - 1 + length;
 		}
 		return endpoint;
 	}
 
+	function checkBounds() {
+		console.log("endpoint", endpoint);
+		if (endpoint < 11) {
+			return true;
+		}
+		return false;
+	}
+
 	const placeShip = (x, y, direction, length) => {
 		setEndpoint(x, y, direction, length);
+		if (!checkBounds()) {
+			throw new Error("Ship is not in bounds");
+		}
+
+		if (direction === "horizontal") {
+			for (let i = 0; i < length; i++) {
+				board[x - 1][y - 1 + i] = true;
+			}
+		} else if (direction === "vertical") {
+			for (let i = 0; i < length; i++) {
+				board[x - 1 + i][y - 1] = true;
+			}
+		}
+		console.table(board);
+		return board;
 	};
 
-	return { placeShip };
+	return { placeShip, board };
 };
 
 export { Gameboard };
