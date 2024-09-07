851666924590bf87de0ec8c683e6b9b94aecdda1
AlphaZer00 <batudasdemir@gmail.com>
2024-09-07 14:52:04 -0400

    Add test for receiveAttack

--- gameboard.test.js
+++ gameboard.test.js
@@ -274,4 +274,49 @@ describe("Gameboard Factory Function", () => {
 			],
 		]);
 	});
+
+	escribe("Gameboard receiveAttack Functionality", () => {
+		let gameBoard;
+
+		beforeEach(() => {
+			// Initialize a new Gameboard
+			gameBoard = Gameboard();
+			// Place a ship at position (1, 1) to (1, 3) horizontally
+			gameBoard.placeShip(1, 1, "horizontal", 3);
+		});
+
+		test("receiveAttack registers a hit when coordinates match a ship", () => {
+			// Attack where a ship is placed
+			gameBoard.receiveAttack(1, 1);
+			expect(gameBoard.board[0][0]).toBe("hit"); // A hit should be registered
+
+			gameBoard.receiveAttack(1, 2);
+			expect(gameBoard.board[0][1]).toBe("hit"); // Another hit should be registered
+
+			gameBoard.receiveAttack(1, 3);
+			expect(gameBoard.board[0][2]).toBe("hit"); // A third hit should be registered
+		});
+
+		test("receiveAttack registers a miss when no ship is at the coordinates", () => {
+			// Attack where no ship is placed
+			gameBoard.receiveAttack(2, 1);
+			expect(gameBoard.missedShots).toContainEqual([2, 1]); // Coordinates should be recorded as a miss
+		});
+
+		test("receiveAttack does not mark coordinates as a miss if a hit was made", () => {
+			gameBoard.receiveAttack(1, 1); // A ship exists at (1,1)
+			expect(gameBoard.missedShots).not.toContainEqual([1, 1]); // It should not be marked as a miss
+		});
+
+		test("multiple missed shots are recorded correctly", () => {
+			gameBoard.receiveAttack(2, 1); // Miss
+			gameBoard.receiveAttack(3, 2); // Miss
+			gameBoard.receiveAttack(4, 5); // Miss
+			expect(gameBoard.missedShots).toEqual([
+				[2, 1],
+				[3, 2],
+				[4, 5],
+			]); // All missed shots should be recorded
+		});
+	});
 });
