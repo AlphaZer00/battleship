871a61d2189751a865df1d2555ee627f10028907
AlphaZer00 <batudasdemir@gmail.com>
2024-09-10 15:35:03 -0400

    Add player factory

---
+++ player.js
@@ -0,0 +1,17 @@
+import { Gameboard } from "./gameboard";
+
+const Player = (type) => {
+	if (type !== "human" && type !== "computer") {
+		throw new Error("Invalid player type");
+	}
+
+	const playerBoard = Gameboard();
+
+	const sendAttack = (opponentBoard, x, y) => {
+		return opponentBoard.receiveAttack(x, y);
+	};
+
+	return { playerBoard, sendAttack };
+};
+
+export { Player };
