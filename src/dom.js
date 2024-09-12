5006c7427570ae4cf802dd2ae8321248bcc3e799
AlphaZer00 <batudasdemir@gmail.com>
2024-09-12 13:23:42 -0400

    Add basic function to display gameboard on html

--- src/dom.js
+++ src/dom.js
@@ -1,2 +1,50 @@
 import { Player } from "./player.js";
 
+const renderBoard = (player) => {
+	// Create reference to board
+	const board = player.playerBoard.board;
+
+	const grid = document.createElement("div");
+};
+
+const createGrid = (player) => {
+	// Create reference to player's gameboard
+	const board = player.playerBoard.board;
+
+	// Create grid container
+	const gridContainer = document.createElement("div");
+	gridContainer.style.display = "grid";
+
+	// Style grid container
+	gridContainer.style.gridTemplateColumns = `repeat(10, 1fr)`;
+	gridContainer.style.gridTemplateRows = `repeat(10, 1fr)`;
+	gridContainer.style.gap = "5px";
+	gridContainer.style.width = "500px";
+	gridContainer.style.height = "500px";
+	gridContainer.classList.add("grid");
+
+	for (let row = 0; row < board.length; row++) {
+		for (let col = 0; col < board[row].length; col++) {
+			// Create square
+			const square = document.createElement("div");
+			square.classList.add("square");
+
+			// Style Square
+			square.style.border = "1px solid #000";
+			square.style.display = "flex";
+			square.style.justifyContent = "center";
+			square.style.alignItems = "center";
+
+			// Determine square content from gameBoard
+			if (board[row][col] === null) {
+				square.textContent = "null";
+			} else {
+				square.textContent = "S";
+			}
+			gridContainer.appendChild(square);
+		}
+	}
+	document.body.appendChild(gridContainer);
+};
+
+export { createGrid };
