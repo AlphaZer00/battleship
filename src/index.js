9b8e604b80a0054a5167f031e7e337b3d1d72f64
AlphaZer00 <batudasdemir@gmail.com>
2024-09-12 13:24:24 -0400

    Add temporary driver code for dom.js function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import { Player } from "./player.js";
+import { Ship } from "./ship.js";
+import { createGrid } from "./dom.js";
+
+//Temporarily, add boats manually without user input
+
+const player1 = Player("human");
+
+const ship3 = Ship(3);
+const ship2 = Ship(2);
+player1.playerBoard.placeShip(1, 1, "horizontal", ship3);
+player1.playerBoard.placeShip(3, 1, "horizontal", ship2);
+
+createGrid(player1);
