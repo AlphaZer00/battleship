97fd9677f74552d22178d8c9ff35fe0d80d5feff
AlphaZer00 <batudasdemir@gmail.com>
2024-09-23 10:00:11 -0400

    Create function for computer sending random attack

--- src/index.js
+++ src/index.js
@@ -12,25 +12,19 @@ import {
 //Temporarily, add boats manually without user input
 
 const player1 = Player("human");
-const player2 = Player("human");
+const player2 = Player("computer");
 
 const ship1 = Ship(1);
 const ship3 = Ship(3);
 const ship2 = Ship(2);
+const ship4 = Ship(4);
 player1.playerBoard.placeShip(1, 1, "horizontal", ship3);
 player1.playerBoard.placeShip(3, 1, "horizontal", ship2);
 player2.playerBoard.placeShip(1, 1, "horizontal", ship1);
+player2.playerBoard.placeShip(2, 2, "horizontal", ship4);
 
 createGrid(player1);
 createOppGrid(player2);
-
-player2.sendAttack(player1.playerBoard, "A", 1);
-player2.sendAttack(player1.playerBoard, "A", 2);
-player2.sendAttack(player1.playerBoard, "A", 3);
-
-displayHit(player1, "A", 1);
-displayHit(player1, "A", 2);
-displayHit(player1, "A", 3);
 
 selectSquare();
 
@@ -38,10 +32,10 @@ selectSquare();
 sendAttackOnClick(player1, player2.playerBoard);
 
 //SEND RANDOM ATTACK
-const attackCoords = player2.computerRandomAttack(player1.playerBoard);
-
-if (attackCoords) {
-	const [x, y] = attackCoords;
-	console.log("randcoords", x, y);
-	displayHit(player1, x, y);
+function computerSendRandomAttack(computer, human) {
+	const attackCoords = computer.computerRandomAttack(human.playerBoard);
+	if (attackCoords) {
+		const [x, y] = attackCoords;
+		displayHit(human, x, y);
+	}
 }
